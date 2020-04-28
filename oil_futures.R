90b02df23148313a6b02db22aff17244e28d1ee1
jonferrin <44932621+jonferrin@users.noreply.github.com>
2020-04-27 20:28:16 -0600

    Made a few more graphs
    Oil futures graph idea

---
+++ oil_futures.R
@@ -0,0 +1,15 @@
+' API KEY = D7CTwvHDyicCWMRDi9_6 "
+
+library(Quandl)
+Quandl.api_key(D7CTwvHDyicCWMRDi9_6)
+from.dat <- as.Date("01/01/2010", format="%d/%m/%Y")
+to.dat <- as.Date("01/01/2020", format="%d/%m/%Y")
+crude.oil.futures<-Quandl("CHRIS/CME_CL1", start_date = from.dat, end_date = to.dat, type="xts")
+plot(crude.oil.futures$Last)
+
+library(Quandl)
+Quandl.api_key(D7CTwvHDyicCWMRDi9_6)
+from.dat <- as.Date("01/01/2010", format="%d/%m/%Y")
+to.dat <- as.Date("01/01/2011", format="%d/%m/%Y")
+crude.oil<-Quandl("CHRIS/CME_CL1", start_date = from.dat, end_date = to.dat, type="xts")
+plot(crude.oil.futures$Last)
