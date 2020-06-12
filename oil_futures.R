b44141700cabdd45f5862f21656cf25a5900b48d
jonferrin <44932621+jonferrin@users.noreply.github.com>
2020-06-11 18:18:52 -0600

    added comments

--- oil_futures.R
+++ oil_futures.R
@@ -1,17 +1,26 @@
 
-
+#API Key for Quandl
 ' API KEY = D7CTwvHDyicCWMRDi9_6 "
+
+#Load Libraries & API key
 library(ggplot)
 library(Quandl)
 Quandl.api_key(D7CTwvHDyicCWMRDi9_6)
+
+#Format dates for Quandl function
 from.dat <- as.Date("01/01/2010", format="%d/%m/%Y")
 to.dat <- as.Date("01/01/2020", format="%d/%m/%Y")
 crude.oil.futures<-Quandl("CHRIS/CME_CL1", start_date = from.dat, end_date = to.dat, type="xts")
-plot(crude.oil.futures$Last)
+oil_plot <- plot(crude.oil.futures$Last)
+print(oil_plot)
+
+
+View(crude.oil.futures)
 
 library(Quandl)
 Quandl.api_key(D7CTwvHDyicCWMRDi9_6)
 from.dat <- as.Date("01/01/2010", format="%d/%m/%Y")
 to.dat <- as.Date("01/01/2011", format="%d/%m/%Y")
 crude.oil<-Quandl("CHRIS/CME_CL1", start_date = from.dat, end_date = to.dat, type="xts")
-plot(crude.oil.futures$Last)
+oil_plot <- plot(crude.oil.futures$Last)
+print(oil_plot)
